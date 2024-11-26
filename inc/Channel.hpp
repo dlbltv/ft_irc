4bc731ede3576da583f6615c82954cba601f9f0e
mortinso <89762267+mortinso@users.noreply.github.com>
2024-11-26 15:57:25 +0000

    Some more function/variable declaration allignment

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 17:26:00 by idelibal          #+#    #+#             */
-/*   Updated: 2024/11/25 19:49:01 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/11/26 15:55:46 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,24 @@
 
 class Channel {
 	private:
-		std::string name;
-		std::map<int, Client*> members; // Key: client FD, Value: Client pointer
-		std::set<int> operators;		// Operator FDs
+		std::string				name;
+		std::map<int, Client*>	members;	// Key: client FD, Value: Client pointer
+		std::set<int>			operators;	// Operator FDs
 
 	public:
 		Channel(const std::string& name);
 		~Channel();
 
-		void addMember(Client* client);
-		void removeMember(Client* client);
-		void broadcast(const std::string& message, Client* sender = NULL);
-		void addOperator(Client* client);
-		bool isOperator(Client* client);
-		bool isMember(Client* client) const;
+		void	addMember(Client* client);
+		void	removeMember(Client* client);
+		void	broadcast(const std::string& message, Client* sender = NULL);
+		void	addOperator(Client* client);
+		bool	isOperator(Client* client);
+		bool	isMember(Client* client) const;
 
 		// Getters
-		const std::string& getName() const;
-		std::string getMemberList() const;
+		const std::string&	getName() const;
+		std::string			getMemberList() const;
 };
 
 #endif // Channel_HPP
